61199520a90b3dd4a76a430a5ffcee971cc62437
Gerald <geraldalfons2546@gmail.com>
2026-02-12 17:21:23 +0700

    week01: (tugas 2) implement null safety for user note

--- src/oop_119185_GeraldAlfons/week01/GameStore.kt
+++ src/oop_119185_GeraldAlfons/week01/GameStore.kt
@@ -1,25 +1,31 @@
-package oop_119185_GeraldAlfons.week01
+    package oop_119185_GeraldAlfons.week01
 
-fun main() {
-    val gameTitle: String = "Elden Ring: Shadow of the Erdtree"
-    val price: Int = 600000
+    fun main() {
+        val gameTitle: String = "Elden Ring: Shadow of the Erdtree"
+        val price: Int = 600000
+        val userNote: string? = null
 
-    val discount = calculateDiscount(price)
-    val finalPrice = price - discount
+        val discount = calculateDiscount(price)
+        val finalPrice = price - discount
 
-    printReceipt(
-        title = gameTitle,
-        finalPrice = finalPrice
-    )
+        printReceipt(
+            title = gameTitle,
+            finalPrice = finalPrice,
+            note = userNote
+        )
 
-}
+    }
 
-fun calculateDiscount(price: Int): Int = if (price > 500000) (price * 0.2).toInt() else (price * 0.1).toInt()
+    fun calculateDiscount(price: Int): Int = if (price > 500000) (price * 0.2).toInt() else (price * 0.1).toInt()
 
-fun printReceipt(title: String, finalPrice: Int) {
-    println("========= STRUK PEMBELIAN =========")
-    println("Game        : $title")
-    println("Total Bayar : Rp $finalPrice")
-    println("===================================")
-    println("Terima kasih telah belanja di SteamKW!")
-}
+    fun printReceipt(title: String, finalPrice: Int, note: String?) {
+        println("========= STRUK PEMBELIAN =========")
+        println("Game        : $title")
+        println("Total Bayar : Rp $finalPrice")
+
+        val displayNote = note ?: "Tidak ada catatan"
+        println("Catatan     : $displayNote")
+
+        println("===================================")
+        println("Terima kasih telah belanja di SteamKW!")
+    }
