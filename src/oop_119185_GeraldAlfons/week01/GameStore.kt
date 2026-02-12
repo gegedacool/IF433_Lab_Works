3b87a7f5c9bdb2bb31fc7f311f369c1ccc7fddbc
Gerald <geraldalfons2546@gmail.com>
2026-02-12 17:18:41 +0700

    week01: (tugas 2) implement receipt printing

--- src/oop_119185_GeraldAlfons/week01/GameStore.kt
+++ src/oop_119185_GeraldAlfons/week01/GameStore.kt
@@ -4,6 +4,22 @@ fun main() {
     val gameTitle: String = "Elden Ring: Shadow of the Erdtree"
     val price: Int = 600000
 
+    val discount = calculateDiscount(price)
+    val finalPrice = price - discount
+
+    printReceipt(
+        title = gameTitle,
+        finalPrice = finalPrice
+    )
+
 }
 
 fun calculateDiscount(price: Int): Int = if (price > 500000) (price * 0.2).toInt() else (price * 0.1).toInt()
+
+fun printReceipt(title: String, finalPrice: Int) {
+    println("========= STRUK PEMBELIAN =========")
+    println("Game        : $title")
+    println("Total Bayar : Rp $finalPrice")
+    println("===================================")
+    println("Terima kasih telah belanja di SteamKW!")
+}
