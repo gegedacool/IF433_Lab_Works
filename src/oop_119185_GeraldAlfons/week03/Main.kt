dbd81893f19246978c71295e100c27a6beccbc56
Gerald <geraldalfons2546@gmail.com>
2026-02-20 15:35:38 +0700

    week03: clean up

--- src/oop_119185_GeraldAlfons/week03/Main.kt
+++ src/oop_119185_GeraldAlfons/week03/Main.kt
@@ -5,12 +5,7 @@ fun main() {
     e.salary = -1000
     e.salary =5000000
     println("Gaji : ${e.salary}")
-
-    // 2. Test Encapsulation
     e.increasePerformance()
-    //e.performanceRating = 5
-
-    //3. Test computed Property
     println("Pajak yang harus dibayar : ${e.salary}")
 }
 
