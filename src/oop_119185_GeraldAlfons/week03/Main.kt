640a152227cdb43798e0ab92ec14f7733540fd25
Gerald <geraldalfons2546@gmail.com>
2026-02-20 15:47:17 +0700

    week03: (task 1) implement Weapon testing in main

--- src/oop_119185_GeraldAlfons/week03/Main.kt
+++ src/oop_119185_GeraldAlfons/week03/Main.kt
@@ -7,5 +7,20 @@ fun main() {
     println("Gaji : ${e.salary}")
     e.increasePerformance()
     println("Pajak yang harus dibayar : ${e.salary}")
+
+    println("\n--- Test Tugas 1: Weapon ---")
+    val sword = Weapon("Excalibur")
+
+    // 1. Test Damage Negatif
+    sword.damage = -50
+    println("Damage sekarang: ${sword.damage}") // Harusnya tetap 0
+
+    // 2. Test Damage > 1000
+    sword.damage = 9999
+    println("Damage sekarang: ${sword.damage}") // Harusnya jadi 1000
+
+    // 3. Test Tier
+    println("Tier Senjata: ${sword.tier}")
+}
 }
 
