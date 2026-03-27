dfed7aea48798867ba6a8fd7e6280191ae0f56f0
unknown <geraldalfons2546@gmail.com>
2026-03-27 18:13:59 +0700

    week07 : (trial) implement companion object as factory in NetworkClient

--- src/oop_119185_GeraldAlfons/Week07/DatabaseManager.kt
+++ src/oop_119185_GeraldAlfons/Week07/DatabaseManager.kt
@@ -1,2 +1,9 @@
-package Week07 
+package oop_119185_GeraldAlfons.Week07
 
+object DatabaseManager {
+    var connectionStatus : String = "Disconnected"
+    fun connect() {
+        connectionStatus = "Connected to server"
+        println("Database is ready.")
+    }
+}
