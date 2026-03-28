0b78e60532c80e5140fbeacdb1a53878ad4bd4ec
unknown <geraldalfons2546@gmail.com>
2026-03-28 10:57:57 +0700

    week07: (task) create GameItem data class

--- src/oop_119185_GeraldAlfons/Week07/GameManager.kt
+++ src/oop_119185_GeraldAlfons/Week07/GameManager.kt
@@ -1,4 +1,14 @@
 package oop_119185_GeraldAlfons.Week07
 
-class GameManager {
+object GameManager {
+    var isGameRunning: Boolean = false
+
+    fun startGame() {
+        if (isGameRunning) {
+            println("Game sudah berjalan! Mencegah instansiasi ganda.") [cite: 139]
+        } else {
+            isGameRunning = true
+            println("Memulai Game Engine...") [cite: 140]
+        }
+    }
 }
--- src/oop_119185_GeraldAlfons/Week07/GameModels.kt
+++ src/oop_119185_GeraldAlfons/Week07/GameModels.kt
@@ -1,4 +1,17 @@
 package oop_119185_GeraldAlfons.Week07
 
-class GameModels {
+enum class ItemRarity (val dropChance: Int){
+    enum class ItemRarity(val dropChance: Int) {
+        COMMON(70),
+        UNCOMMON(30),
+        RARE(10),
+        EPIC(5),
+        LEGENDARY(1) [cite: 145, 146]
+    }
 }
+
+data class GameItem(
+    val name: String,
+    val damage: Int,
+    val rarity: ItemRarity
+) [cite: 151, 152]
