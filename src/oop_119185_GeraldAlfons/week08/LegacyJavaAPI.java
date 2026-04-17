704b7c49852fb9daef2f196970c46f4700d04b00
unknown <geraldalfons2546@gmail.com>
2026-04-17 23:37:47 +0700

    week08: implement java interop using !!

---
+++ src/oop_119185_GeraldAlfons/week08/LegacyJavaAPI.java
@@ -0,0 +1,7 @@
+package oop_119185_GeraldAlfons.week08
+
+public class LegacyJavaAPI {
+    public static String fetchServerStatus() {
+        return "SERVER_ONLINE_200_OK";
+    }
+}
--- src/oop_119185_GeraldAlfons/week08/Main.kt
+++ src/oop_119185_GeraldAlfons/week08/Main.kt
@@ -55,4 +55,8 @@ fun Main {
         println(e.message)
     }
 
+    println("\n=== TEST JAVA INTEROP ===")
+    val javaResponse = LegacyJavaAPI.fetchServerStatus()
+    val statusLength = javaResponse!!.length
+    println("Status dari Java: $javaResponse (Length: $statusLength)")
 }
