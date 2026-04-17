7e7e4725e22c7d8ce7215c8fa8bcb3e32dcc41c1
unknown <geraldalfons2546@gmail.com>
2026-04-18 00:04:22 +0700

    week08: (task) test full e-commerce pipeline in main

--- src/oop_119185_GeraldAlfons/week08/ECommerceMain.kt
+++ src/oop_119185_GeraldAlfons/week08/ECommerceMain.kt
@@ -1,4 +1,26 @@
 package oop_119185_GeraldAlfons.week08
 
-class ECommerceMain {
+fun main() {
+    val rawApiData: List<Map<String, Any?>> = listOf(
+        mapOf("id" to "E01", "name" to "Laptop", "type" to "ELECTRONIC", "warranty" to 24),
+        mapOf("id" to "C01", "name" to "T-Shirt", "type" to "CLOTHING", "size" to "XL"),
+        mapOf("id" to "E02", "name" to "Mouse", "type" to "ELECTRONIC", "warranty" to "Not An Integer"),
+        mapOf("name" to "Ghost Item", "type" to "CLOTHING"), // Missing ID!
+        mapOf("id" to "X01", "name" to "Unknown", "type" to "FOOD") // Unknown type
+    )
+
+    val parser = ApiParser()
+
+    for (raw in rawApiData) {
+        try {
+            val product = parser.parseProduct(raw)
+            product?.let {
+                println("Processing: ${it}")
+                parser.checkout(it)
+            }
+        } catch (e: IllegalArgumentException) {
+            println("DATA CORRUPT: ${e.message}")
+        }
+    }
+    //tested
 }
