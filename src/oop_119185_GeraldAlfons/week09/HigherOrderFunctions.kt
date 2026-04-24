acf030661c694078bfb757fb39982ad02daeb4d5
unknown <geraldalfons2546@gmail.com>
2026-04-24 15:42:27 +0700

    week09: implement map HOF for data transformation

--- src/oop_119185_GeraldAlfons/week09/HigherOrderFunctions.kt
+++ src/oop_119185_GeraldAlfons/week09/HigherOrderFunctions.kt
@@ -1,4 +1,16 @@
 package oop_119185_GeraldAlfons.week09
 
-class HigherOrderFunctions {
+fun main() {
+    val numbers = listOf(1, 2, 3, 4, 5, 6, 7, 8)
+    println("Original Data: $numbers")
+
+    println("\n=== HOF: FILTER ===")
+    val evens = numbers.filter { it % 2 == 0 }
+    println("Evens only: $evens")
+
+    println("\n=== HOF: MAP ===")
+    val multiplied = evens.map { it * 10 }
+    val asStrings = multiplied.map { "Rp ${it}k" }
+    println("Multiplied: $multiplied")
+    println("Formatted: $asStrings")
 }
--- src/oop_119185_GeraldAlfons/week09/LambdaPractice.kt
+++ src/oop_119185_GeraldAlfons/week09/LambdaPractice.kt
@@ -1,2 +1,10 @@
 package oop_119185_GeraldAlfons.week09
 
+fun main() {
+    println("=== TEST LAMBDA ===")
+    val sumLambda = { a: Int, b: Int -> a + b }
+    println("Hasil Sum: ${sumLambda(5, 10)}")
+
+    val squareImplicit: (Int) -> Int = { it * it }
+    println("Hasil Square: ${squareImplicit(4)}")
+}
