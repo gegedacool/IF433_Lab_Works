eb649a1b584f5bdb34269ea2c66254568961b083
unknown <geraldalfons2546@gmail.com>
2026-05-06 23:21:38 +0700

    week09: (task) filter CLOSED trades only

--- src/oop_119185_GeraldAlfons/week09/TradingDashboard.kt
+++ src/oop_119185_GeraldAlfons/week09/TradingDashboard.kt
@@ -1,4 +1,12 @@
 package oop_119185_GeraldAlfons.week09
 
-class TradingDashboard {
-}
+fun main() {
+    val tradeHistory = listOf(
+        TradeLog("BTCUSDT", "LONG", 20, 15.5, "CLOSED"),
+        TradeLog("ETHUSDT", "SHORT", 10, -5.2, "CLOSED"),
+        TradeLog("BTCUSDT", "SHORT", 50, 120.0, "OPEN"),
+        TradeLog("SOLUSDT", "LONG", 10, 8.4, "CLOSED"),
+        TradeLog("ETHUSDT", "LONG", 20, -12.0, "CLOSED"),
+        TradeLog("BTCUSDT", "LONG", 25, -2.5, "CLOSED")
+    )
+    val closedTrades = tradeHistory.filter { it.status == "CLOSED" }
--- src/oop_119185_GeraldAlfons/week09/TradingModels.kt
+++ src/oop_119185_GeraldAlfons/week09/TradingModels.kt
@@ -1,4 +1,9 @@
 package oop_119185_GeraldAlfons.week09
 
-class TradingModels {
-}
+data class TradeLog(
+    val pair: String,
+    val position: String,
+    val leverage: Int,
+    val roe: Double,
+    val status: String
+)
