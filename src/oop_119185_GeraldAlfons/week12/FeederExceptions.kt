3d4c8daf5c2218f1047d035f3a4d14fa662e6783
unknown <geraldalfons2546@gmail.com>
2026-05-15 22:50:12 +0700

    week12: (task) implement hardware validation with throw

--- src/oop_119185_GeraldAlfons/week12/FeederExceptions.kt
+++ src/oop_119185_GeraldAlfons/week12/FeederExceptions.kt
@@ -1,8 +1,8 @@
 package oop_119185_GeraldAlfons.week12
 
 sealed class FeederException(msg: String) : Exception(msg)
-
 class FoodEmptyException(requested: Int, available: Int) :
     FeederException("Kibble tidak cukup! Diminta $requested gr, sisa $available gr")
+
 class DispenserJamException :
     FeederException("Wadah dispenser tersangkut/macet!")
--- src/oop_119185_GeraldAlfons/week12/SmartFeeder.kt
+++ src/oop_119185_GeraldAlfons/week12/SmartFeeder.kt
@@ -1,4 +1,8 @@
 package oop_119185_GeraldAlfons.week12
 
-class SmartFeeder {
+fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): Int {
+    require(requestedGram > 0) { "Porsi kibble harus lebih dari 0 gr" }
+    if (isJammed) {
+        throw DispenserJamException()
+    }
 }
