ca290789a40bbed6f03e24e0c192372703afc3ee
unknown <geraldalfons2546@gmail.com>
2026-05-15 22:27:43 +0700

    week11: (task) configure AC unit using run

--- src/oop_119185_GeraldAlfons/week11/SmartHomeDashboard.kt
+++ src/oop_119185_GeraldAlfons/week11/SmartHomeDashboard.kt
@@ -14,4 +14,9 @@ fun main() {
         homeDevices.add(it)
     }
 
+    val acUnit = run {
+        SmartDevice("Daikin Inverter (Kabel 3x2.5)", "HVAC", false, 800)
+    }
+    homeDevices.add(acUnit)
+    homeDevices.add(SmartDevice("Picolo's Auto Feeder", "Pet Care", true, 10))
 }
