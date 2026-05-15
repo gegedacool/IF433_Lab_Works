ccd43c53e4093d41806202a769d56825fd14fab4
unknown <geraldalfons2546@gmail.com>
2026-05-15 15:02:44 +0700

    week10: test math operations and comparable consstraints

--- src/oop_119185_GeraldAlfons/week10/NumericalStorage.kt
+++ src/oop_119185_GeraldAlfons/week10/NumericalStorage.kt
@@ -1,11 +1,18 @@
 package oop_119185_GeraldAlfons.week10
 
 class MathBox<T : Number>(val value1: T, val value2: T) {
-    fun sum() : Double {
+
+    fun sum(): Double {
         return value1.toDouble() + value2.toDouble()
     }
-    fun <T> getMax(a: T, b: T) : T where T : Comparable<T> {
-        return if (a>b) a else b
+
+    fun <T> getMax(a: T, b: T): T where T : Comparable<T> {
+        return if (a > b) a else b
     }
+    println("\n=== TEST CONSTRAINTS ===")
+    val math = MathBox(10.5, 20)
+    println("Total: ${math.sum()}")
+    println("Terbesar: ${getMax(45,90)}")
+}
 }
 
