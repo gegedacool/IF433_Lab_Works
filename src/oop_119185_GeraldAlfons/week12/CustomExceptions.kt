9850fe62139e54753312b31177b49995af80c42e
unknown <geraldalfons2546@gmail.com>
2026-05-15 22:41:47 +0700

    week12: implement throw keyword for validation

--- src/oop_119185_GeraldAlfons/week12/CustomExceptions.kt
+++ src/oop_119185_GeraldAlfons/week12/CustomExceptions.kt
@@ -1,4 +1,18 @@
 package oop_119185_GeraldAlfons.week12
 
-class CustomExceptions {
+class InsufficientFundsException(
+    val amount: Double,
+    val balance: Double
+) : Exception("Attempted $amount, balance: $balance")
+class BankAccount(var balance: Double) {
+    fun withdraw(amount: Double) {
+        if (amount < 0) {
+            throw IllegalArgumentException("Amount must be positive")
+        }
+        if (amount > balance) {
+            throw InsufficientFundsException(amount, balance)
+        }
+        balance -= amount
+        println("Withdrawal successful. Remaining balance: $balance")
+    }
 }
